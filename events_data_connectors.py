0c71a06fb683d297ff41eb323224fa4c4d8d9487
yaroslav <you@example.com>
2024-02-11 11:31:22 +0200

    added MetronomoOptimismTXBigQueryConnector

--- events_data_connectors.py
+++ events_data_connectors.py
@@ -1,1 +1,48 @@
-# Here will be data connector for Optimism events data
+from abc import ABC, abstractmethod
+import pandas as pd
+from dotenv import load_dotenv
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+class DataConnector(ABC):
+    @abstractmethod
+    def getData(self) -> pd.DataFrame:
+        pass
+
+
+class MetronomoOptimismTXBigQueryConnector(DataConnector):
+    def __init__(self,
+        dates,
+        project,
+        dataset,
+        token_json_path=None):
+        """
+        Parameters
+        ----------
+        dates: list[datetime.Date]
+            dates range to retrieve the data. Should be iterable of datetime.date type
+        project: str
+            name of the project to get data from. Either provided or got from .static_env file, variable MetronomoOptimismTXBigQueryConnector_DEFAULT_PROJECT
+        dataset: str
+            name of the dataset to get data from. Either provided or got from .static_env file, variable MetronomoOptimismTXBigQueryConnector_DEFAULT_dataset
+        token_json_path: str
+            path to token json file. Either provided or got from .env file, variable MetronomoTXCloudStorageConnector_TOKEN_JSON_PATH
+        """
+
+        load_dotenv("user_similarity_optimism_calculator/static_config.env")
+
+        self.token_json_path = token_json_path
+        self.credentials = service_account.Credentials.from_service_account_file(self.token_json_path)
+        self.client = bigquery.Client(credentials= self.credentials,project=project)
+
+        self.query_t = """
+            SELECT * 
+            FROM `optimism_data.v_transactions_full_30`
+            """
+        
+
+    def getData(self):
+        query_job = client.query(query_t)
+        transactions = query_job.result().to_dataframe().drop('date_', axis=1).groupby(['from_address', 'to_address']).sum()
+        transactions.columns = ["from_address", "to_address", "interactions_num"]
+        return transactions
